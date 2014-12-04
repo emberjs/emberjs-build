b0998f4e4ab25a2e990e30ca816f9d07452a69eb
Alex Navasardyan <twokul@users.noreply.github.com>
2014-12-04 16:29:41 -0500

    Adds build config

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var vendoredPackage    = require('./lib/vendored-package');
 var es6VendoredPackage = require('./lib/es6-vendored-package');
 var htmlbarsPackage    = require('./lib/htmlbars-package');
 var defeatureifyConfig = require('./lib/defeatureify-config');
+var buildConfig        = require('./lib/build-config');
 
 module.exports = {
   vendoredPackage:    vendoredPackage,
   es6VendoredPackage: es6VendoredPackage,
   htmlbarsPackage:    htmlbarsPackage,
-  defeatureifyConfig: defeatureifyConfig
+  defeatureifyConfig: defeatureifyConfig,
+  buildConfig:        buildConfig
 };
