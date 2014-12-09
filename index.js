8a5ac49747f53da4f4f966104a7a54d1489afde1
Alex Navasardyan <twokul@users.noreply.github.com>
2014-12-09 12:03:44 -0500

    Exports packages functions

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var getEmberDevTestHelpers = require('./lib/ember-dev-test-helpers');
 var getVendoredPackages    = require('./lib/get-vendored-packages');
 var getS3TestRunner        = require('./lib/s3-test-runner');
 
+var vendoredPackage        = require('./lib/vendored-package');
+var htmlbarsPackage        = require('./lib/htmlbars-package');
+var vendoredES6Package     = require('./lib/es6-vendored-package');
+
 module.exports = {
   defeatureifyConfig:     defeatureifyConfig,
   buildConfig:            buildConfig,
@@ -15,5 +19,8 @@ module.exports = {
   getTestConfigTree:      testConfigTree,
   getBowerTree:           getBowerTree,
   getEmberDevTestHelpers: getEmberDevTestHelpers,
-  getS3TestRunner:        getS3TestRunner
+  getS3TestRunner:        getS3TestRunner,
+  vendoredPackage:        vendoredPackage,
+  htmlbarsPackage:        htmlbarsPackage,
+  vendoredES6Package:     vendoredES6Package
 };
