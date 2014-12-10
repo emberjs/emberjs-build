dadf6fc01b94571ac0f7b5d7b59343888c968988
Alex Navasardyan <twokul@users.noreply.github.com>
2014-12-10 14:17:38 -0500

    es6Package and concatES6

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var getS3TestRunner          = require('./lib/s3-test-runner');
 var packages                 = require('./lib/packages');
 var getPackageDependencyTree = require('./lib/get-package-dependency-tree');
 
+var concatES6                = require('./concat-es6');
+var es6Package               = require('get-es6-package');
+
 module.exports = {
   defeatureifyConfig:       defeatureifyConfig,
   buildConfig:              buildConfig,
@@ -19,5 +22,7 @@ module.exports = {
   getEmberDevTestHelpers:   getEmberDevTestHelpers,
   getS3TestRunner:          getS3TestRunner,
   packages:                 packages,
-  getPackageDependencyTree: getPackageDependencyTree
+  getPackageDependencyTree: getPackageDependencyTree,
+  concatES6:                concatES6,
+  es6Package:               es6Package
 };
