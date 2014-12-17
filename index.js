0eba5857eec4e453a1ab32ae0a4c0ad19a15c62c
Alex Navasardyan <twokul@users.noreply.github.com>
2014-12-17 12:25:05 -0500

    zomg

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 'use strict';
 
-var defeatureifyConfig       = require('./lib/defeatureify-config');
-var buildConfig              = require('./lib/build-config');
-var testConfigTree           = require('./lib/test-config-tree');
-var getBowerTree             = require('./lib/bower-tree');
-var getEmberDevTestHelpers   = require('./lib/ember-dev-test-helpers');
-var getVendoredPackages      = require('./lib/get-vendored-packages');
-var getS3TestRunner          = require('./lib/s3-test-runner');
-var packages                 = require('./lib/packages');
+var EmberBuild = require('./lib/ember-build');
 
-var concatES6                = require('./lib/concat-es6');
-var es6Package               = require('./lib/get-es6-package');
-var buildRuntimeTree         = require('./lib/get-build-runtime-tree');
-
-module.exports = {
-  defeatureifyConfig:      defeatureifyConfig,
-  buildConfig:             buildConfig,
-  getVendoredPackages:     getVendoredPackages,
-  getTestConfigTree:       testConfigTree,
-  getBowerTree:            getBowerTree,
-  getEmberDevTestHelpers:  getEmberDevTestHelpers,
-  getS3TestRunner:         getS3TestRunner,
-  packages:                packages,
-  concatES6:               concatES6,
-  es6Package:              es6Package,
-  buildRuntimeTree:        buildRuntimeTree
-};
+module.exports = EmberBuild;
