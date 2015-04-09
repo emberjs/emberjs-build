4b872ece7e852eabb797b056d6144384b111058c
Sean Doyle <sean.p.doyle24@gmail.com>
2015-04-08 20:51:57 -0400

    Fix failing test

--- tests/expected/htmlbars/htmlbars-test-helpers.js
+++ tests/expected/htmlbars/htmlbars-test-helpers.js
@@ -1,1 +1,1 @@
-define("htmlbars-test-helpers",  ["exports"],  function(__exports__) {    "use strict";    function foo() {    }    __exports__.foo = foo;function bar() {    }    __exports__.bar = bar;  });
+define('htmlbars-test-helpers', ['exports'], function (exports) {	'use strict';	exports.foo = foo;	exports.bar = bar;	function foo() {}	function bar() {}});
