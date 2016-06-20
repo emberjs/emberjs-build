021f65bf2c352ee090858bf64076a196af06d487
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-06-20 22:25:11 +0200

    Update expected output files

--- tests/expected/transpile-es6/conditional-use-strict/with-no-use-strict-directive.js
+++ tests/expected/transpile-es6/conditional-use-strict/with-no-use-strict-directive.js
@@ -1,6 +1,6 @@
-enifed("with-no-use-strict-directive", ["exports"], function (exports) {
+enifed("with-no-use-strict-directive", [], function () {
   "no use strict";
 
   function nothingHere() {}
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndpdGgtbm8tdXNlLXN0cmljdC1kaXJlY3RpdmUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLGlCQUFlLENBQUM7O0FBRWhCLFdBQVMsV0FBVyxHQUFHLEVBQUciLCJmaWxlIjoid2l0aC1uby11c2Utc3RyaWN0LWRpcmVjdGl2ZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIlwibm8gdXNlIHN0cmljdFwiO1xuXG5mdW5jdGlvbiBub3RoaW5nSGVyZSgpIHsgfVxuIl19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndpdGgtbm8tdXNlLXN0cmljdC1kaXJlY3RpdmUuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBOztBQUVBLFdBQVMsV0FBVCxHQUF1QixDQUFHIiwiZmlsZSI6IndpdGgtbm8tdXNlLXN0cmljdC1kaXJlY3RpdmUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJcIm5vIHVzZSBzdHJpY3RcIjtcblxuZnVuY3Rpb24gbm90aGluZ0hlcmUoKSB7IH1cbiJdfQ==
