005e1ec4ce382da07b63d159e95270d3a90eeda2
Trent Willis <twillis@linkedin.com>
2017-04-13 13:30:03 -0700

    Update fixtures to pass CI

--- tests/expected/transpile-es6/conditional-use-strict/without-directive.js
+++ tests/expected/transpile-es6/conditional-use-strict/without-directive.js
@@ -3,4 +3,4 @@ enifed("without-directive", ["exports"], function (exports) {
 
   function nothingHere() {}
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndpdGhvdXQtZGlyZWN0aXZlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLFdBQVMsV0FBVyxHQUFHLEVBQUciLCJmaWxlIjoid2l0aG91dC1kaXJlY3RpdmUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBub3RoaW5nSGVyZSgpIHsgfVxuIl19
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndpdGhvdXQtZGlyZWN0aXZlLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLFdBQVMsV0FBVyxHQUFHLEVBQUciLCJmaWxlIjoid2l0aG91dC1kaXJlY3RpdmUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBub3RoaW5nSGVyZSgpIHsgfVxuIl19
